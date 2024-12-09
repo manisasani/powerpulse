8f082fc790e2f1eb1f1381eaa52d9d674e54ee5d
mani <manisasani2005@gmail.com>
2024-12-09 17:06:12 +0330

    Complete Home Page Layout

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -178,5 +178,27 @@
     // سایر کدها و توابع دیگر...
 
   });
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+          return [...document.querySelectorAll(el)]
+      } else {
+          return document.querySelector(el)
+      }
+  }
 
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+      AOS.init({
+          duration: 1000,
+          easing: 'ease-in-out',
+          once: true,
+          mirror: false
+      });
+  });
 })();
